3e95521733e91ef2873dc1b05fa2dcda7c7e831f
xcaretac <xcaretac@gmail.com>
2016-07-02 01:37:17 -0500

    Primer Commit

--- Swift03Semana01Error/ViewController.swift
+++ Swift03Semana01Error/ViewController.swift
@@ -10,6 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var datosLibro: UITextView!
+    @IBOutlet weak var ISBN: UITextField!
+
+    func asincrono(){
+        let valorISBN=ISBN.text
+        let urls = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:"+valorISBN!
+        let url = NSURL(string: urls)
+        let sesion = NSURLSession.sharedSession()
+        let bloque = { (datos:NSData?, resp: NSURLResponse?, error: NSError?) -> Void in
+            if resp==nil {
+                print("El servidor no existe / No hay red")
+            }else{
+                let texto = NSString(data: datos!, encoding: NSUTF8StringEncoding)
+                dispatch_async(dispatch_get_main_queue()){
+                    self.datosLibro.text=String(texto)
+                }
+            }
+        }
+
+        let dt = sesion.dataTaskWithURL(url!, completionHandler: bloque)
+        dt.resume()
+    }
+    
+    
+
+    @IBAction func consultarLibro(sender: AnyObject) {
+        asincrono()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
